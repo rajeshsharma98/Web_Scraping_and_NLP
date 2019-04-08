2ad0f615f52049cd595262ec93f7859b5b50c7af
rajesh sharma <rajesh.mandi@yahoo.com>
2019-04-08 11:40:58 +0530

    User Input isEnabled
    Now user have to give input of the topic that is to be searched.

--- files/scrap.py
+++ files/scrap.py
@@ -21,16 +21,17 @@ class color:
 class working:
     def general(self,):
         #getting url
-        self.url="https://en.wikipedia.org/wiki/"
-        self.name="Mandi_district"
-        self.url=self.url+self.name
-        self.req = requests.get(self.url)
+        self.url="https://en.wikipedia.org/wiki/Main_page"
+        name=input("Enter the topic: ")
+        post_data = {"search": name}
+        #getting page of required topic
+        self.req = requests.get(self.url, params=post_data)
         self.content = self.req.content
         #applying beautiful soup ( html parsing)
         self.soup = BeautifulSoup(self.content,"html.parser")
         self.all_h=self.soup.find_all("span",{"class":"mw-headline"})
-        self.all_h=self.all_h[:-5] 
-        
+        self.all_h=self.all_h[:-5]
+
     def writing(self):
         file = open("info.txt","a")
 
@@ -50,7 +51,7 @@ class working:
         #to remove "[any_number]" from text
         for i in file:
             filer.write(re.sub("[\(\[].*?[\)\]]", "", i))
-        
+
         print("information fetched")
         os.remove("info.txt")
 
@@ -61,14 +62,14 @@ class working:
          for h in first_s:
              file_sum.write(h.text+"\n"+"X----XX----XX----XX----XX----XX----XX----XX----XX----XX----XX----XX----XX----XX----XX----X"+"\n\n")
          """ to print the intro under heading of the topic still remaining"""
-        
+
          for h in self.all_h:
               # file.write(color.BOLD+h.text+color.END)  ## to make text bold
               file_sum.write("\n"+h.text)
               file_sum.write("\n")
               file_sum.write(summary.use.fun(h.find_next("p").text))
               file_sum.write("\n")
-        
+
          file_sum.close()
          filesr_sum = open("sum.txt","r")
          filer_sum=open("summary.txt","w")
@@ -79,14 +80,12 @@ class working:
          os.remove("sum.txt")
 
 def main():
+    w=working()
+    w.general()
     print("1.Information")
     print("2.Summary")
     a=int(input())
     print("\n")
-    
-    w=working()
-    w.general()
-    
     if a==1:
         w.writing()
     if a==2:
